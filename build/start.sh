19fa6ac88340bc0cb4836c003a882758e1daf955
4piu <16668056+4piu@users.noreply.github.com>
2022-12-02 18:42:34 +0800

    Fix /hath/data/ not exists

--- build/start.sh
+++ build/start.sh
@@ -8,6 +8,7 @@ kill_jar() {
 }
 
 if [ $CLIENT_ID ] && [ $CLIENT_KEY ]; then
+    mkdir -p /hath/data/
     echo -n "${CLIENT_ID}-${CLIENT_KEY}" > /hath/data/client_login
 else if [ ! -f /hath/data/client_login ]; then
     echo "Please specify the CLIENT_ID and CLIENT_KEY, exiting..."
